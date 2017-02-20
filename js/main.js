b8f643bc9c2f6d866c69efe4e2138e39d52ee882
jls47 <jls47@users.noreply.github.com>
2017-02-19 22:53:09 -0800

    Committings!

--- js/main.js
+++ js/main.js
@@ -6,3 +6,64 @@
 // 3. Connect the validation object to an event handler tied to the submit button.
 
 // Refer to the `index.html` file for the validation rules that must be enforced.
+$(document).on('ready', function(){
+	$('#order-form').validate({
+		submitHandler: function(form) {
+			form.submit();
+		},
+		rules: {	
+			"your-name": {
+				maxlength: 128,
+				required: true,
+				minlength: 2
+			},
+			"your-address": {
+				maxlength: 128,
+				required: true,
+				minlength: 5,
+
+			},
+			"your-city": {
+				maxlength: 128,
+				required: true,
+				minlength: 2,
+
+			},
+			"your-state": {
+				maxlength: 2,
+				required: true,
+				minlength: 2,
+
+			},
+			"your-zip": {
+				maxlength: 5,
+				required: true,
+				minlength: 5,
+				number: true
+				
+			},
+			"card-holder-name": {
+				maxlength: 128,
+				required: true,
+				minlength: 2,
+				letters: true,
+				digits: false
+			},
+			"card-number": {
+				required: true,
+				digits: true,
+				maxlength: 19
+			},
+			"cvv": {
+				maxlength: 3,
+				minlength: 3,
+				number: true
+			},
+			"comments": {
+				maxlength: 500
+			} 
+		}
+		
+	});
+	$('[data-toggle="tooltip"]').tooltip();
+});
