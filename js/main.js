0857d39818cc0dc05316e42657cd66453bfec22d
jls47 <jls47@users.noreply.github.com>
2017-02-19 23:13:33 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,14 +6,22 @@
 // 3. Connect the validation object to an event handler tied to the submit button.
 
 // Refer to the `index.html` file for the validation rules that must be enforced.
+
+//When the document is ready,
 $(document).on('ready', function(){
+	//create the validation function
 	$('#order-form').validate({
+		//Tell the submit button to do its job when clicked
 		submitHandler: function(form) {
 			form.submit();
 		},
+		//creating rules for submissions.  If the button is clicked and these are not fulfilled, the user will receive a notification.
 		rules: {	
+			//For example, if
 			"your-name": {
+				//is less than
 				maxlength: 128,
+				//characters long, it's all good.  But more than that, it's getting a little weird.
 				required: true,
 				minlength: 2
 			},
@@ -29,12 +37,14 @@ $(document).on('ready', function(){
 				minlength: 2,
 
 			},
+			//Keeping the state names to two letters.
 			"your-state": {
 				maxlength: 2,
 				required: true,
 				minlength: 2,
 
 			},
+			//Keeping the zip codes to 5 digits.
 			"your-zip": {
 				maxlength: 5,
 				required: true,
@@ -42,6 +52,7 @@ $(document).on('ready', function(){
 				number: true
 				
 			},
+			//Keeping names to letters.
 			"card-holder-name": {
 				maxlength: 128,
 				required: true,
@@ -49,11 +60,13 @@ $(document).on('ready', function(){
 				letters: true,
 				digits: false
 			},
+			//Keeping numbers to numbers.
 			"card-number": {
 				required: true,
 				digits: true,
 				maxlength: 19
 			},
+			//And so on.
 			"cvv": {
 				maxlength: 3,
 				minlength: 3,
@@ -65,5 +78,6 @@ $(document).on('ready', function(){
 		}
 		
 	});
+	//Turning on the tooltips.
 	$('[data-toggle="tooltip"]').tooltip();
 });
